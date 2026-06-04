a2da9e568db939309e03755d89d5f29d32908e2a
harsh-5001 <jasoliyaharsh777@gmail.com>
2026-06-04 12:06:10 +0000

    Export from Misaki Studio.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,11 +30,11 @@ export default ({
         divProps?.ref?.(element);
       }}
     >
-      <div className={` ${id} ${styles["E3"]}`}>
-        <div className={` ${id} ${styles["E4"]}`}>
-          <span className={` ${id} ${styles["E5"]}`}>{"jasoliya"}</span>
-        </div>
-      </div>
+      <div className={` ${id} ${styles["E3"]}`} />
+      <div className={` ${id} ${styles["E4"]}`} />
+      <div className={` ${id} ${styles["E5"]}`} />
+      <div className={` ${id} ${styles["E6"]}`} />
+      <div className={` ${id} ${styles["E7"]}`} />
     </div>
   );
 };
